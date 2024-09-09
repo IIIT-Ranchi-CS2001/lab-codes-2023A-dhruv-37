921027d472bcc9ae69713c71a9bd34762f5d4980
dhruv-37 <136002768+dhruv-37@users.noreply.github.com>
2024-09-09 22:49:33 +0530

    Add files via upload

---
+++ 1-BasicOperations.py
@@ -0,0 +1,26 @@
+a = float(input("Enter a number : "))
+b = float(input("Enter a number : "))
+
+# Addition of two numbers
+c = a + b
+print("Addition of two numbers is", c)
+
+# Subtraction of two numbers
+d = a - b
+print("Subtraction of two numbers is", d)
+
+# Multiplication of two numbers
+e = a * b
+print("Multiplication of two numbers is", e)
+
+# Fraction Quotient of two numbers
+f = a / b
+print("Division of two numbers is", f)
+
+# Integer Quotient of two numbers
+g = a // b
+print("Integer Division of two numbers is", g)
+
+# Remainder of two numbers
+h = a % b
+print("Remainder of two numbers is", h)
